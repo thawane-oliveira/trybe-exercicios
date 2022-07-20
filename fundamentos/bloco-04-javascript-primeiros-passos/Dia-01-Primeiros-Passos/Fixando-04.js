bd2c9b09b022aa5d53eee67dc637d0af837a5a7a
Thawane <thawaneapoliveira@gmail.com>
2022-07-20 18:25:35 -0300

    Adiciona Fixando os aprendizados 4

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-01-Primeiros-Passos/Fixando-04.js
@@ -0,0 +1,13 @@
+const Thawane = 89;
+
+if (Thawane >= 80) {
+    console.log("Parabéns, você foi aprovado(a)");
+}
+
+else if (Thawane < 80 && Thawane >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+
+else {
+    console.log("Você foi reprovado(a)");
+}
