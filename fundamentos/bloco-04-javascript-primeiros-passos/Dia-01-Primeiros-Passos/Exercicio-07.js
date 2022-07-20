3da038fb1e461cd906bb3d80c82105c37f583871
Thawane <thawaneapoliveira@gmail.com>
2022-07-20 20:32:41 -0300

    Adiciona Exercícios 7 e 8

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-01-Primeiros-Passos/Exercicio-07.js
@@ -0,0 +1,30 @@
+let Porcentagem = 45
+
+if (Porcentagem >= 90) {
+    console.log("A");
+}
+   
+else if (Porcentagem >= 80) {
+    console.log("B");
+}
+
+else if (Porcentagem >= 70) {
+    console.log("C");
+}
+
+else if (Porcentagem >= 60) {
+    console.log("D");
+}
+
+else if (Porcentagem >= 50) {
+    console.log("E");
+}
+
+else if (Porcentagem < 50) {
+    console.log("F")
+}
+
+else {
+    console.log("Erro")
+}
+  
