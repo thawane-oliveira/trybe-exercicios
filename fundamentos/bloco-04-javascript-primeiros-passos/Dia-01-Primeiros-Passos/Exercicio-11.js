d39d71c8c3fb36797b02c01af7017c98326f8e80
Thawane <thawaneapoliveira@gmail.com>
2022-07-21 10:39:09 -0300

    Corrige exercício 11 do dia 4.1 de JS

--- fundamentos/bloco-04-javascript-primeiros-passos/Dia-01-Primeiros-Passos/Exercicio-11.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-01-Primeiros-Passos/Exercicio-11.js
@@ -1,8 +1,30 @@
-const salarioBruto = 4000;
-const inss = 360;
-const salarioBase = 3640;
-const aliquotaIR = 273;
-const parcelaFixa = 354.80;
-const valorIR = 81.80;
+let INSS;
+let aliquotaIR;
 
-console.log(salarioBase - valorIR);
+const salarioBruto = 4000.00;
+
+if (salarioBruto <= 1556.94) {
+    INSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+    INSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+    INSS = salarioBruto * 0.11;
+} else {
+    INSS = 570.88;
+}
+
+const salarioBase = salarioBruto - INSS;
+
+if (salarioBase <= 1903.98) {
+    aliquotaIR = 0;
+} else if (salarioBase <= 2826.65) {
+    aliquotaIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+    aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else {
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
+
+console.log("SalárioLíquido: " + (salarioBase - aliquotaIR));
