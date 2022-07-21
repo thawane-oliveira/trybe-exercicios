99eb049f5f2e46a200ae6c668c128412813a8686
Thawane <thawaneapoliveira@gmail.com>
2022-07-21 00:30:29 -0300

    Adiciona exercício 11

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-01-Primeiros-Passos/Exercicio-11.js
@@ -0,0 +1,8 @@
+const salarioBruto = 4000;
+const inss = 360;
+const salarioBase = 3640;
+const aliquotaIR = 273;
+const parcelaFixa = 354.80;
+const valorIR = 81.80;
+
+console.log(salarioBase - valorIR);
