e9076bc70a2be932e0bdfe8bb0767d4058daa484
Thawane <thawaneapoliveira@gmail.com>
2022-07-21 15:04:34 -0300

    Adiciona Fixando conteúdo 04

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-02-Array-e-Loop-For/Fixando-Aprendizados/Fixando-04.js
@@ -0,0 +1,7 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+groceryList.push ('Goiaba');
+
+for (let index = 0; index < groceryList.length; index +=1) {
+    console.log(groceryList[index]);
+}
