ee66312d62b410989575434c557e009e27ea902d
Thawane <thawaneapoliveira@gmail.com>
2022-07-22 19:25:58 -0300

    Adiciona bonus 01

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-03-Logica-de-Programacao-e-Algoritmos/Bonus-01.js
@@ -0,0 +1,10 @@
+let n = 6;
+let asterisco = '*';
+let linha = '';
+
+for (let index = 0; index < n; index +=1 ) {
+linha = linha + asterisco;
+};
+for (let index = 0; index < n; index += 1) {
+    console.log(linha);
+};
