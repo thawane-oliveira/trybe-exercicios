870a428ae0b1945e93ce73a992588397d5ef0d18
Thawane <thawaneapoliveira@gmail.com>
2022-07-27 20:55:38 -0300

    Adiciona correção

--- fundamentos/bloco-04-javascript-primeiros-passos/Dia-02-Array-e-Loop-For/Fixando-Aprendizados/Teste.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/Dia-02-Array-e-Loop-For/Fixando-Aprendizados/Teste.js
@@ -1,13 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+const n = "9" ;
 
-// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
-
-let higherNumber = numbers[0];
-
-for (let index = 1; index < numbers.length; index += 1) {
-  if (numbers[index] > higherNumber) {
-    higherNumber = numbers[index];
-  }
-}
-
-console.log(higherNumber);
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
