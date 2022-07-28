bc5d692e6b0453494c15e7cb856e476f828545c1
Thawane <thawaneapoliveira@gmail.com>
2022-07-28 18:27:44 -0300

    Adiciona Fixando aprendizados 02 e 03

--- fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-02-trabalhando-elementos/Fixando-aprendizados/script.js
+++ fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-02-trabalhando-elementos/Fixando-aprendizados/script.js
@@ -1,17 +1,69 @@
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// Exercicio 01
 
-const pai = elementoOndeVoceEsta.parentElement;
-pai.style.color = 'violet';
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
-const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
-primeiroFilhoDoFilho.innerText = 'Primeiro filho do filho';
+// const pai = elementoOndeVoceEsta.parentElement;
+// pai.style.color = 'violet';
 
-const primeiroFilho = pai.firstElementChild;
+// const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+// primeiroFilhoDoFilho.innerText = 'Primeiro filho do filho';
 
-const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
+// const primeiroFilho = pai.firstElementChild;
 
-const texto = elementoOndeVoceEsta.nextSibling;
+// const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
 
-const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+// const texto = elementoOndeVoceEsta.nextSibling;
 
-const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+// const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+
+// const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+
+// Exercicio 02
+
+// const pai = document.getElementById('pai');
+
+// const irmaoElementoOndeVoceEsta = document.createElement('section');
+
+// irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+
+// pai.appendChild(irmaoElementoOndeVoceEsta);
+
+// //
+
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+// const filhoElementoOndeVoceEsta = document.createElement('div');
+
+// filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+
+// elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+// //
+
+// const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+
+// const filhoPrimeiroFilhoDoFilho = document.createElement('div');
+
+// filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
+
+// primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+
+// //
+
+// const terceiroFilho = filhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+// console.log((terceiroFilho));
+
+
+// // Exercicio 03
+
+// const pai = document.getElementById('pai');
+
+//     for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+//       const currentChildren = pai.childNodes[index];
+//       if (currentChildren.id !== 'elementoOndeVoceEsta') {
+//         currentChildren.remove();
+//       }
+//     }
+
+//     const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+//     segundoEUltimoFilhoDoFilho.remove();
