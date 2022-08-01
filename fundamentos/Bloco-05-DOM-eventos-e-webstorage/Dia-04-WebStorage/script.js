43bb59a154d570619b8654e1cec96940c4159619
Thawane <thawaneapoliveira@gmail.com>
2022-08-01 20:52:09 -0300

    Adiciona localStorage dos itens alterados

--- fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
+++ fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
@@ -1,6 +1,7 @@
 function mudaCor(){
-    const corDoFundo = document.querySelector('select').value;
+    const corDoFundo = document.querySelector('#mudaFundo').value;
     document.querySelector('body').style.backgroundColor = corDoFundo;
+    localStorage.setItem("backgroundColor", corDoFundo);
 } 
 
 document.querySelector('#mudaFundo').addEventListener('change', mudaCor)
@@ -10,6 +11,7 @@ document.querySelector('#mudaFundo').addEventListener('change', mudaCor)
 function mudaCorTexto(){
     const corDaLetra = document.querySelector('#corTexto').value;
     document.querySelector('body').style.color = corDaLetra;
+    localStorage.setItem("fontColor", corDaLetra);
 }
 document.querySelector('#corTexto').addEventListener('change', mudaCorTexto)
 
@@ -18,6 +20,7 @@ document.querySelector('#corTexto').addEventListener('change', mudaCorTexto)
 function tamanhoFonte(){
     const tamanho = document.querySelector('#tamanhoFonte').value;
     document.querySelector('body').style.fontSize = tamanho;
+    localStorage.setItem("fontSize", tamanho);
 }
 
 document.querySelector('#tamanhoFonte').addEventListener('change', tamanhoFonte)
@@ -27,6 +30,7 @@ document.querySelector('#tamanhoFonte').addEventListener('change', tamanhoFonte)
 function espacamento(){
     const espaco = document.querySelector('#espacoLinha').value;
     document.querySelector('body').style.lineHeight = espaco;
+    localStorage.setItem("lineHeight", espaco);
 }
 document.querySelector('#espacoLinha').addEventListener('change', espacamento)
 
@@ -35,6 +39,7 @@ document.querySelector('#espacoLinha').addEventListener('change', espacamento)
 function style(){
     const estilo = document.querySelector('#tipoFonte').value;
     document.querySelector('body').style.fontFamily = estilo;
+    localStorage.setItem("fontStyle", estilo);
 }
 document.querySelector('#tipoFonte').addEventListener('change', style)
 
