2f90822ebdf151754c4f920cdd3fe3e35811ef54
Thawane <thawaneapoliveira@gmail.com>
2022-08-01 20:05:03 -0300

    Adiciona tamanho de fonte e espaçamento entre linhas

--- fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
+++ fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
@@ -12,3 +12,20 @@ function mudaCorTexto(){
     document.querySelector('body').style.color = corDaLetra;
 }
 document.querySelector('#corTexto').addEventListener('change', mudaCorTexto)
+
+//
+
+function tamanhoFonte(){
+    const tamanho = document.querySelector('#tamanhoFonte').value;
+    document.querySelector('body').style.fontSize = tamanho;
+}
+
+document.querySelector('#tamanhoFonte').addEventListener('change', tamanhoFonte)
+
+//
+
+function espacamento(){
+    const espaco = document.querySelector('#espacoLinha').value;
+    document.querySelector('p').style.lineHeight = espaco;
+}
+document.querySelector('#espacoLinha').addEventListener('change', espacamento)
