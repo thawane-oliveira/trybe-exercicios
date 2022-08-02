b3c384210a076773772be797484977ba8eb0f036
Thawane <thawaneapoliveira@gmail.com>
2022-08-02 11:13:25 -0300

    Adiciona recuperação do localStorage ao reiniciar ou recarregar página

--- fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
+++ fundamentos/Bloco-05-DOM-eventos-e-webstorage/Dia-04-WebStorage/script.js
@@ -1,5 +1,5 @@
-function mudaCor(){
-    const corDoFundo = document.querySelector('#mudaFundo').value;
+window.onload = function(){function mudaCor(){
+    let corDoFundo = document.querySelector('#mudaFundo').value;
     document.querySelector('body').style.backgroundColor = corDoFundo;
     localStorage.setItem("backgroundColor", corDoFundo);
 } 
@@ -9,7 +9,7 @@ document.querySelector('#mudaFundo').addEventListener('change', mudaCor)
 //
 
 function mudaCorTexto(){
-    const corDaLetra = document.querySelector('#corTexto').value;
+    let corDaLetra = document.querySelector('#corTexto').value;
     document.querySelector('body').style.color = corDaLetra;
     localStorage.setItem("fontColor", corDaLetra);
 }
@@ -18,7 +18,7 @@ document.querySelector('#corTexto').addEventListener('change', mudaCorTexto)
 //
 
 function tamanhoFonte(){
-    const tamanho = document.querySelector('#tamanhoFonte').value;
+    let tamanho = document.querySelector('#tamanhoFonte').value;
     document.querySelector('body').style.fontSize = tamanho;
     localStorage.setItem("fontSize", tamanho);
 }
@@ -28,7 +28,7 @@ document.querySelector('#tamanhoFonte').addEventListener('change', tamanhoFonte)
 //
 
 function espacamento(){
-    const espaco = document.querySelector('#espacoLinha').value;
+    let espaco = document.querySelector('#espacoLinha').value;
     document.querySelector('body').style.lineHeight = espaco;
     localStorage.setItem("lineHeight", espaco);
 }
@@ -37,9 +37,30 @@ document.querySelector('#espacoLinha').addEventListener('change', espacamento)
 //
 
 function style(){
-    const estilo = document.querySelector('#tipoFonte').value;
+    let estilo = document.querySelector('#tipoFonte').value;
     document.querySelector('body').style.fontFamily = estilo;
     localStorage.setItem("fontStyle", estilo);
 }
 document.querySelector('#tipoFonte').addEventListener('change', style)
+
+//
+
+function recuperandoDados(){
+    let corDoFundoSalva = localStorage.getItem("backgroundColor");
+    let body = document.querySelector('body');
+    body.style.backgroundColor = corDoFundoSalva;
+
+        let corDaLetraSalva = localStorage.getItem("fontColor");
+        body.style.color = corDaLetraSalva;
+
+        let tamanhoSalvo = localStorage.getItem("fontSize");
+        body.style.fontSize = tamanhoSalvo;
+
+        let espacoSalvo = localStorage.getItem("lineHeight");
+        body.style.lineHeight = espacoSalvo;
+
+        let estiloSalvo = localStorage.getItem("fontStyle");
+        body.style.fontFamily = estiloSalvo;
+} recuperandoDados();
+}
 
