2904a51b8634ceaec71460adedac16d10bfef60f
Thawane <thawaneapoliveira@gmail.com>
2022-08-05 10:46:24 -0300

    Adiciona escutador de evento nos exercicios 2 e 3

--- fundamentos/Bloco-06-Forms-Flexbox-e-responsivo/script.js
+++ fundamentos/Bloco-06-Forms-Flexbox-e-responsivo/script.js
@@ -1,10 +1,21 @@
-const submit = document.querySelector('#sendButton');
-const clearAll = document.querySelector('#clear');
-
 function youCantSend (event) {
   event.preventDefault();
 }
 
-window.onload = function () {
+function clear() {
+  const inputs = document.querySelectorAll('input');
+  const texts = document.querySelector('textarea');
+  for (let index = 0; index < inputs.length; index += 1) {
+    const textUser = inputs[index];
+    textUser.value = '';
+    textUser.checked = false;
+  }
+  texts.value = '';
+}
+
+window.onload() = function () {
+  const submit = document.querySelector('#sendButton');
   submit.addEventListener('click', youCantSend);
+  const clearAll = document.querySelector('#clear');
+  clearAll.addEventListener('click', clear);
 };
