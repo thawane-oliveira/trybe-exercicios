b3b01c04b2b8c9a4006de49dcf78f17f28bcf093
Thawane <thawaneapoliveira@gmail.com>
2022-08-16 19:38:13 -0300

    Adiciona exercicio 2

---
+++ fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-03-Primeiros-passos-em-Jest/myRemove.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('./myRemove.js');
+
+describe('teste da função myRemove', () => {
+  it ('retorna array removendo item discriminado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
--- fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-03-Primeiros-passos-em-Jest/sum.js
+++ fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-03-Primeiros-passos-em-Jest/sum.js
@@ -8,4 +8,3 @@ function sum(a, b) {
 
 module.exports = sum;
 
-x
