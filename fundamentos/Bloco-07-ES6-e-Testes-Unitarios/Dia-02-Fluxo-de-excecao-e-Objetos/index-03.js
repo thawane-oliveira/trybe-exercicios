39d9cb1c1e5fea6a9407e758717b8bfef651fcd7
Thawane <thawaneapoliveira@gmail.com>
2022-08-16 18:31:20 -0300

    Adiciona partes 2 a 5 do exercicio 3

--- fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-02-Fluxo-de-excecao-e-Objetos/index-03.js
+++ fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-02-Fluxo-de-excecao-e-Objetos/index-03.js
@@ -22,5 +22,27 @@ const newKey = (object, key, value) => {
   object[key] = value;
 };
 
+//
+
 newKey(lesson2, 'turno', 'noite');
 console.log(lesson2);
+
+//
+
+const listOfKeys = (object) => Object.keys(object);
+console.log(listOfKeys(lesson1));
+
+//
+
+const objectSize = (object) => Object.keys(object).length;
+console.log(objectSize(lesson1));
+
+//
+
+const valueList = (object) => Object.values(object);
+console.log(valueList(lesson1));
+
+//
+
+const lessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(lessons);
