f0bbaeb25bdb917e505dc7f14eca6ec3f24327d1
Thawane <thawaneapoliveira@gmail.com>
2022-08-16 18:47:08 -0300

    Adiciona bônus

--- fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-02-Fluxo-de-excecao-e-Objetos/index-03.js
+++ fundamentos/Bloco-07-ES6-e-Testes-Unitarios/Dia-02-Fluxo-de-excecao-e-Objetos/index-03.js
@@ -46,3 +46,66 @@ console.log(valueList(lesson1));
 
 const lessons = Object.assign({}, {lesson1, lesson2, lesson3});
 console.log(lessons);
+
+//
+
+const numberOfStudents = (object) => {
+  let total = 0;
+  const keys = Object.keys(object);
+  for (index in keys) {
+    total += object[keys[index]].numeroEstudantes;
+  }
+  return total;
+};
+
+console.log((numberOfStudents(lessons)));
+
+//
+
+const keyValue = (object, number) => Object.values(object) [number];
+console.log(keyValue(lesson1, 0));
+
+//
+
+// const verifyPair = (object, key, value) => {
+//   const values = Object.entries(object);
+//   let 
+// };
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+
+//
+
+const mathStudents = (object) => {
+  let total = 0;
+  const array = Object.keys(object);
+  for (index in array) {
+    if(object[array[index]].materia === 'Matemática'){
+    total += object[array[index]].numeroEstudantes;
+    }
+  }
+  return total;
+}
+console.log(mathStudents(lessons));
+
+//
+
+const lessonAndStudent = (object, name) => {
+  const lessons = [];
+  let students = 0;
+  const array = Object.values(object);
+  for (index in array) {
+    if (array[index].professor === name) {
+      lessons.push(array[index].materia)
+      students += array[index].numeroEstudantes;
+    }
+  }
+  return { aulas: lessons, estudantes: students };
+}
+
+const relatorio = (lessons, name) => {
+  const relacao = {};
+  relacao.professor = name;
+  Object.assign(relacao, lessonAndStudent(lessons, name));
+  return relacao;
+}
+console.log(relatorio(lessons, 'Maria Clara'));
