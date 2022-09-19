d0b638e336471c7cc31a29c40f9a147096acef69
Thawane <thawaneapoliveira@gmail.com>
2022-09-19 19:05:46 -0300

    Adiciona exercício do dia

---
+++ front-end/Bloco-10-React/Dia-01-Hellow-World-no-React/exercicio-01/src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {'Thawane'}
+        </h1>
+        <p>
+          {'Tenho 26 anos e amo desenhar'}
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>{'HTML'}</li>
+          <li>{'CSS'}</li>
+          <li>{'JS'}</li>                    
+        </ul>
+      </div>
+    );
+  }
+}
+export default About;
