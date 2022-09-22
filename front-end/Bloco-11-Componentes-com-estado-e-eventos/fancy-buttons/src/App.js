d6be7d37b054e10b0c1008b7175b892c342c6614
Thawane <thawaneapoliveira@gmail.com>
2022-09-22 18:23:21 -0300

    Adiciona pasta bloco 11

---
+++ front-end/Bloco-11-Componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+    <div>
+      <button>Botão 1</button>
+      <button>Botão 2</button>
+      <button>Botão 3</button>
+    </div>
+    )
+  }
+}
+
+export default App;
