865fe9ee346559f3240e363035d43c265b884131
Thawane <thawaneapoliveira@gmail.com>
2023-01-13 14:21:56 -0300

    Adiciona exercício 5 e 6

---
+++ back-end/Secao-01-Docker/Dia-03-Docker-Compose/Exercicio-02/my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> anasdasdasdsad save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          xablau<br />
+          
+          Thawane
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
