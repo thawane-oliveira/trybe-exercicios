8ef2260a372a2455e797d56d1d6302b323910aa8
Thawane <thawaneapoliveira@gmail.com>
2023-02-02 20:29:41 -0300

    adiciona exercicio 01, bora fazer o 2

---
+++ back-end/Secao-04-Introducao-ao-desenvolvimento-com-Node-JS/Dia-04-Express-e-middlewares/exercicio-01/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateDescription = require('./middlewares/validateDescription');
+const validateDifficulty = require('./middlewares/validateDifficulty');
+const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validatePrice');
+const validateRating = require('./middlewares/validateRating');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/activities',
+validateDifficulty,
+validateRating,
+validateCreatedAt,
+validateDescription,
+validatePrice,
+validateName,
+(_req, res) => {
+  res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
+
+module.exports = app;
