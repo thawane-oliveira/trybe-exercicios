5375e16f56b106c03b583abeffd655b0c69bc802
Thawane <thawaneapoliveira@gmail.com>
2023-02-27 19:06:40 -0300

    finaliza exercício guiado

--- back-end/Secao-06-ORM-e-autenticacao/Dia-01-Interface-de-aplicacao/app-with-sequelize/src/models/user.model.js
+++ back-end/Secao-06-ORM-e-autenticacao/Dia-01-Interface-de-aplicacao/app-with-sequelize/src/models/user.model.js
@@ -2,16 +2,10 @@ const UserModel = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phoneNum: DataTypes.STRING,
   });
   (async () => {
     await sequelize.sync({ force: true });
-    const sara = User.build({
-      fullName: 'Sara Silva Santos',
-      email: 'sara.ss@trybe.com',
-    });
-    
-    console.log(sara instanceof User);
-    console.log(sara.fullName);
   })();
 
   return User;
